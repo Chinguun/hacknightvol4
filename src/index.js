39dfad196e6f1650496c396c10020dbc373af429
Chinguun <it.chinguun@gmail.com>
2019-02-28 20:30:46 +0800

    Update source code 2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const buildDate = process.env.APP_BUILD_DATE || new Date().toISOString()
 
 app.get('/', (req, res) => {
     const welcome = myLib.helloWorld()
-    const text = `${welcome}! CommitID ${commitRef} Үүсгэгдсэн хугацаа ${buildDate}`
+    const text = `${welcome}! уу ! CommitID ${commitRef} Үүсгэгдсэн хугацаа ${buildDate}`
     res.send(text)
 })
 
